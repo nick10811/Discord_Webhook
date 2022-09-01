14e1b3f2a5edea4d841cc40d5828d0959e40be36
Nick Yang <nick10811@gmail.com>
2022-09-01 20:45:06 +0800

    adjust conding style

--- run.sh
+++ run.sh
@@ -8,8 +8,7 @@ TAG=""
 AVATAR=""
 COLOR=15844367
 
-display_help()
-{
+display_help() {
    echo "Usage: ./run.sh [options...]"
    echo "-u, --url            The url of webhook (requirement)"
    echo "-t, --title          The title"
@@ -25,59 +24,57 @@ display_help()
    exit 1
 }
 
-while :
-do
+while :; do
    case "$1" in
-      "-u" | "--url")
-         URL=$2
-         shift 2
-         ;;
-      "-t" | "--title")
-         TITLE=$2
-         shift 2
-         ;;
-      "-m" | "--message")
-         MESSAGE=$2
-         shift 2
-         ;;
-      "-h" | "--help")
-         display_help
-         exit 0
-         ;;
-      "--username")
-         USERNAME=$2
-         shift 2
-         ;;
-      "--tag")
-         TAG=$2
-         shift 2
-         ;;
-      "--avatar")
-         AVATAR=$2
-         shift 2
-         ;;
-      "--color")
-         COLOR=$2
-         shift 2
-         ;;
-      -*)
-          echo "Error: Unknown option: $1" >&2
-          echo "try './run.sh --help' for more information"
-          exit 1 
-          ;;
-      *) # No more options
-         break
-         ;;
+   "-u" | "--url")
+      URL=$2
+      shift 2
+      ;;
+   "-t" | "--title")
+      TITLE=$2
+      shift 2
+      ;;
+   "-m" | "--message")
+      MESSAGE=$2
+      shift 2
+      ;;
+   "-h" | "--help")
+      display_help
+      exit 0
+      ;;
+   "--username")
+      USERNAME=$2
+      shift 2
+      ;;
+   "--tag")
+      TAG=$2
+      shift 2
+      ;;
+   "--avatar")
+      AVATAR=$2
+      shift 2
+      ;;
+   "--color")
+      COLOR=$2
+      shift 2
+      ;;
+   -*)
+      echo "Error: Unknown option: $1" >&2
+      echo "try './run.sh --help' for more information"
+      exit 1
+      ;;
+   *) # No more options
+      break
+      ;;
    esac
 done
 
-if [ -z "$URL" ]
-then 
-    display_help
-    exit 1
+if [ -z "$URL" ]; then
+   display_help
+   exit 1
 fi
 
 OBJ="{\"title\":\"$TITLE\", \"type\": \"rich\", \"color\": $COLOR, \"description\": \"$MESSAGE\"}"
 curl -X POST $URL \
--H 'Content-Type: application/json' \
--d "{ \"avatar_url\": \"$AVATAR\", \"username\": \"$USERNAME\", \"content\": \"$TAG\", \"embeds\": [$OBJ] }"
+   -H 'Content-Type: application/json' \
+   -d "{ \"avatar_url\": \"$AVATAR\", \"username\": \"$USERNAME\", \"content\": \"$TAG\", \"embeds\": [$OBJ] }"
